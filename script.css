151ca430e39b8ad1ede4fbadd7ec565abca70a7e
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2020-04-20 22:33:53 +0530

    Add files via upload

---
+++ script.css
@@ -0,0 +1,12 @@
+function animateIcon() {
+    var info = document.getElementById("info");
+    info.innerHTML = "&#xf129;";
+    
+    setTimeout(function(){
+        info.innerHTML = "&#xf05a;";
+    }, 1000);
+}
+
+animateIcon();
+
+setInterval(animateIcon, 3000);
